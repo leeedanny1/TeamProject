75ae1f09d550227b7e1894e0c611046e7d60f441
leeedanny1 <leeedanny1@gmail.com>
2021-11-30 16:07:09 +0900

    공지사항 불러오기

--- JCMarket/workspace/jcmarket/src/main/java/com/springboot/jcmarket/web/service/NoticeService.java
+++ JCMarket/workspace/jcmarket/src/main/java/com/springboot/jcmarket/web/service/NoticeService.java
@@ -1,10 +1,13 @@
 package com.springboot.jcmarket.web.service;
 
 import com.springboot.jcmarket.domain.notice.Notice;
-import com.springboot.jcmarket.web.dto.notice.NoticeDto;
+import com.springboot.jcmarket.web.dto.notice.GetNoticeDto;
 
 public interface NoticeService {
 
+//	전체 공지사항 가져옴
 	public Notice getNoticeAll();
-	
+
+//	공지사항 디테일 페이지
+	public Notice getNotice(int notice_code);
 }
--- JCMarket/workspace/jcmarket/src/main/java/com/springboot/jcmarket/web/service/NoticeServiceImpl.java
+++ JCMarket/workspace/jcmarket/src/main/java/com/springboot/jcmarket/web/service/NoticeServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.springboot.jcmarket.domain.notice.Notice;
 import com.springboot.jcmarket.domain.notice.NoticeRepository;
-import com.springboot.jcmarket.web.dto.notice.NoticeDto;
+import com.springboot.jcmarket.web.dto.notice.GetNoticeDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +17,11 @@ public class NoticeServiceImpl implements NoticeService {
 	@Override
 	public Notice getNoticeAll() {
 		return noticeRepository.getNoticeAll();
+	}
+	
+	@Override
+	public Notice getNotice(int notice_code) {
+		return noticeRepository.getNotice(notice_code);
 	}
 
 }
